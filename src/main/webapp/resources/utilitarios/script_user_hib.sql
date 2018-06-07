17188a2032ccc9bc1ea37d074488e7045ea15fde
jmichaels1 <michael.salr1590@gmail.com>
2018-06-07 12:26:44 +0200

    add script db

---
+++ src/main/webapp/resources/utilitarios/script_user_hib.sql
@@ -0,0 +1,38 @@
+create database user_hib;
+use user_hib;
+
+drop table access;
+drop table user;
+
+create table user(
+    email varchar(50) primary key,
+    name varchar(50),
+    lastname varchar(50),
+    enterprise varchar(50),
+    registerdate varchar(50)
+);
+
+create table access(
+	email varchar(50),
+    date varchar(50),
+    time varchar(50),
+    PRIMARY KEY (email, date, time),
+    FOREIGN KEY (email) REFERENCES user(email)
+);
+
+-- insert into user values ('michael@gmail.com', 'michael', 
+-- 'salinas', 'megaautos', '05/06/2018');
+
+-- insert into access values ('michael@gmail.com', '05/06/2018', '23:14:15');
+
+-- select * from user;
+-- select * from access;
+
+-- truncate access;
+-- truncate user;
+
+-- SET FOREIGN_KEY_CHECKS = 0; 
+-- TRUNCATE table user; 
+-- SET FOREIGN_KEY_CHECKS = 1
+
+
